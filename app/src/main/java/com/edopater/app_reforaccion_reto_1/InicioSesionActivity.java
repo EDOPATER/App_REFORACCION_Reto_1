2c586ab01982da76197639efa7b2b8110cd2c26f
EDOPATER <edopater@gmail.com>
2024-07-08 22:53:53 -0500

    Actualización MainActivity

---
+++ app/src/main/java/com/edopater/app_reforaccion_reto_1/InicioSesionActivity.java
@@ -0,0 +1,16 @@
+package com.edopater.app_reforaccion_reto_1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InicioSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
--- app/src/main/java/com/edopater/app_reforaccion_reto_1/MainActivity.java
+++ app/src/main/java/com/edopater/app_reforaccion_reto_1/MainActivity.java
@@ -1,11 +1,16 @@
 package com.edopater.app_reforaccion_reto_1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button Registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +18,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Button Registro = findViewById(R.id.buttonRegistro);
 
-
+        Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(MainActivity.this,registrarseActivity.class);
+                startActivity(next);
+            }
+        });
     }
 }
