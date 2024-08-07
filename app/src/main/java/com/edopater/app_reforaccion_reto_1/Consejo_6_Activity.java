1c7d360afa68a2a758c3d30892acf985ec547c8a
EDOPATER <edopater@gmail.com>
2024-08-06 21:21:11 -0500

    Desarrollo Vista Consejo 6.

---
+++ app/src/main/java/com/edopater/app_reforaccion_reto_1/Consejo_6_Activity.java
@@ -0,0 +1,72 @@
+package com.edopater.app_reforaccion_reto_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
+import android.net.Uri;
+
+public class Consejo_6_Activity extends AppCompatActivity {
+
+    private VideoView videoRefor;
+    private Button A_Consejos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consejo6);
+        VideoView videoRefor = findViewById(R.id.videoRefor);
+
+        if (videoRefor != null) {
+            Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_reforestar);
+            videoRefor.setVideoURI(videoUri);
+            videoRefor.start();
+        } else {
+            Log.e("Consejo_1_Activity", "VideoView with ID 'miVideoView' not found");
+        }
+
+        // Enlaza el TextView
+        TextView consejoTextView = findViewById(R.id.Consejo6);
+
+        // Asigna un texto al TextView
+        consejoTextView.setText("o\tMonitoreo regular: Inspecciona regularmente los árboles para detectar signos de plagas y enfermedades.\n\n" +
+                "o\tTratamiento temprano: Toma medidas rápidas para controlar y tratar plagas y enfermedades tan pronto como se detecten.\n\n");
+
+        String text = "o\tMonitoreo regular: Inspecciona regularmente los árboles para detectar signos de plagas y enfermedades.\n\n" +
+                "o\tTratamiento temprano: Toma medidas rápidas para controlar y tratar plagas y enfermedades tan pronto como se detecten.\n\n";
+
+        SpannableString spannableString = new SpannableString(text);
+
+        String boldText1 = "Monitoreo regular:";
+        String boldText2 = "Tratamiento temprano:";
+
+        int start1 = text.indexOf(boldText1);
+        int end1 = start1 + boldText1.length();
+        spannableString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), start1, end1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        int start2 = text.indexOf(boldText2);
+        int end2 = start2 + boldText2.length();
+        spannableString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), start2, end2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        consejoTextView.setText(spannableString);
+        consejoTextView.setText(spannableString);
+        A_Consejos = findViewById(R.id.buttonVolverConsejos);
+
+        A_Consejos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                Intent back = new Intent(Consejo_6_Activity.this, ConsejosActivity.class);
+                startActivity(back);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/edopater/app_reforaccion_reto_1/ConsejosActivity.java
+++ app/src/main/java/com/edopater/app_reforaccion_reto_1/ConsejosActivity.java
@@ -76,7 +76,7 @@ public class ConsejosActivity extends AppCompatActivity {
             }
         });
 
-        /*A_Plagas = findViewById(R.id.buttonPlagas);
+        A_Plagas = findViewById(R.id.buttonPlagas);
 
         A_Plagas.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,7 +86,7 @@ public class ConsejosActivity extends AppCompatActivity {
             }
         });
 
-        A_Podas = findViewById(R.id.buttonPodas);
+        /*A_Podas = findViewById(R.id.buttonPodas);
 
         A_Podas.setOnClickListener(new View.OnClickListener() {
             @Override
